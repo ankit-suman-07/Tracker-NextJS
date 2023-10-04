addf2d98d160d174a86019fb355d80ad7b773e56
ankit-suman-07 <ankitsuman07@gmail.com>
2023-10-04 12:45:18 +0530

    Checking window.typedef

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -6,67 +6,70 @@ import "../css/LineChart.css";
 const CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 const LineChart = (props) => {
-    const amountArr = props.amount;
-    const dateArr = props.date;
+    if (typeof window !== "undefined") {
+        const amountArr = props.amount;
+        const dateArr = props.date;
 
-    const monthTotals = {};
+        const monthTotals = {};
 
-    dateArr.forEach((dateString, index) => {
-        const amount = amountArr[index];
-        const date = new Date(dateString);
+        dateArr.forEach((dateString, index) => {
+            const amount = amountArr[index];
+            const date = new Date(dateString);
 
-        const year = date.getFullYear();
-        const month = date.getMonth();
+            const year = date.getFullYear();
+            const month = date.getMonth();
 
-        const monthKey = `${year}-${month}`;
+            const monthKey = `${year}-${month}`;
 
-        if (monthTotals[monthKey]) {
-            monthTotals[monthKey] += amount;
-        } else {
-            monthTotals[monthKey] = amount;
+            if (monthTotals[monthKey]) {
+                monthTotals[monthKey] += amount;
+            } else {
+                monthTotals[monthKey] = amount;
+            }
+        });
+
+        const sortedMonthKeys = Object.keys(monthTotals).sort();
+
+        const dataArr = sortedMonthKeys.map(monthKey => {
+            const [year, month] = monthKey.split('-');
+            return {
+                x: new Date(Number(year), Number(month), 1),
+                y: monthTotals[monthKey]
+            };
         }
-    });
+        )
 
-    const sortedMonthKeys = Object.keys(monthTotals).sort();
+        const options = {
+            animationEnabled: true,
 
-    const dataArr = sortedMonthKeys.map(monthKey => {
-        const [year, month] = monthKey.split('-');
-        return {
-            x: new Date(Number(year), Number(month), 1),
-            y: monthTotals[monthKey]
-        };
-    }
-    )
-
-    const options = {
-        animationEnabled: true,
-
-        // title: {
-        //     text: "Monthly Sales - 2017"
-        // },
-        axisX: {
-            valueFormatString: "MMM",
-        },
-        axisY: {
-            // title: "Sales (in USD)",
-            prefix: "$"
-        },
-        width: 450,
-        height: 280,
-        backgroundColor: "#ededed",
-        data: [{
-            yValueFormatString: "$#,###",
-            xValueFormatString: "MMMM",
-            type: "spline",
-            dataPoints: dataArr,
-        }]
+            // title: {
+            //     text: "Monthly Sales - 2017"
+            // },
+            axisX: {
+                valueFormatString: "MMM",
+            },
+            axisY: {
+                // title: "Sales (in USD)",
+                prefix: "$"
+            },
+            width: 450,
+            height: 280,
+            backgroundColor: "#ededed",
+            data: [{
+                yValueFormatString: "$#,###",
+                xValueFormatString: "MMMM",
+                type: "spline",
+                dataPoints: dataArr,
+            }]
+        }
+        return (
+            <div className='line-graph-comp' >
+                {CanvasJSChart && <CanvasJSChart options={options} />}
+            </div>
+        );
     }
 
-    return (
-        <div className='line-graph-comp' >
-            {CanvasJSChart && <CanvasJSChart options={options} />}
-        </div>
-    );
+
 };
 
 export default LineChart;
